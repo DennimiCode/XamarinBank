226d577f8cc6a51668e15c1f8367ba0be247e818
Denis Denisov <prostolink200@gmail.com>
2021-10-12 23:59:49 +0300

    first commit

---
+++ XamarinBank/XamarinBank/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace XamarinBank
+{
+    public partial class MainPage : ContentPage
+    {
+        private TapGestureRecognizer _registerLabelTap = new TapGestureRecognizer();
+        public MainPage()
+        {
+            InitializeComponent();
+            _registerLabelTap.Tapped += (sender, args) =>
+            {
+                Application.Current.MainPage = new Pages.RegistrationPage();
+            };
+            RegisterLabel.GestureRecognizers.Add(_registerLabelTap);
+        }
+    }
+}
---
+++ XamarinBank/XamarinBank/Pages/RegistrationPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinBank.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegistrationPage : ContentPage
+    {
+        public RegistrationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void GoBackButtonOnClick(object sender, EventArgs e)
+        {
+            Application.Current.MainPage = new MainPage();
+        }
+    }
+}
